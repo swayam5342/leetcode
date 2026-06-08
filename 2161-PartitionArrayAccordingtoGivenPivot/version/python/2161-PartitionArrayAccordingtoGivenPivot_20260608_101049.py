8fe624a8bdf09809cd1fe0ea23250e82622b28f6
SWAYAM <157891924+swayam5342@users.noreply.github.com>
2026-06-08 10:10:49 +0530

    Create 2161-PartitionArrayAccordingtoGivenPivot

---
+++ 2161-PartitionArrayAccordingtoGivenPivot/version/python/2161-PartitionArrayAccordingtoGivenPivot_20260608_101049.py
@@ -0,0 +1,15 @@
+# Last updated: 6/8/2026, 10:10:49 AM
+1class Solution:
+2    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+3        l=[]
+4        eq=[]
+5        h=[]
+6        count = 0
+7        for i in nums:
+8            if i < pivot:
+9                l.append(i)
+10            elif i > pivot:
+11                h.append(i)
+12            else:
+13                eq.append(i)
+14        return l + eq + h
