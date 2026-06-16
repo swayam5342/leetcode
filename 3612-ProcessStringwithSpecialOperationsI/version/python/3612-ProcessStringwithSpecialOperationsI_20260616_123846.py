43861e297650419c8e0fec956838e7f749f25c6c
SWAYAM <157891924+swayam5342@users.noreply.github.com>
2026-06-16 12:38:39 +0530

    Create 3612-ProcessStringwithSpecialOperationsI

---
+++ 3612-ProcessStringwithSpecialOperationsI/version/python/3612-ProcessStringwithSpecialOperationsI_20260616_123846.py
@@ -0,0 +1,16 @@
+# Last updated: 6/16/2026, 12:38:46 PM
+1class Solution:
+2    def processStr(self, s: str) -> str:
+3        r = []
+4
+5        for ch in s:
+6            if ch == "#":
+7                r.extend(r)
+8            elif ch == "*":
+9                if r:
+10                    r.pop()
+11            elif ch == "%":
+12                r.reverse()
+13            else:
+14                r.append(ch)
+15        return "".join(r)
