ec8fb31f73d0615776e06725f628395182685d81
SWAYAM <157891924+swayam5342@users.noreply.github.com>
2026-06-19 15:39:34 +0530

    Create 1732-FindtheHighestAltitude

---
+++ 1732-FindtheHighestAltitude/version/python/1732-FindtheHighestAltitude_20260619_153945.py
@@ -0,0 +1,9 @@
+# Last updated: 6/19/2026, 3:39:45 PM
+1class Solution:
+2    def largestAltitude(self, gain: List[int]) -> int:
+3        count = 0
+4        ma = 0
+5        for i in gain:
+6            count+=i
+7            ma =max(count,ma)
+8        return ma
